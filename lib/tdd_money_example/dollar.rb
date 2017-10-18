a9008bb24b173f2247e2d3c7bd1553e1a3171688
ikoba <i.koba.jp@gmail.com>
2017-10-18 22:22:08 +0900

    Finish chapter 4

--- lib/tdd_money_example/dollar.rb
+++ lib/tdd_money_example/dollar.rb
@@ -1,12 +1,13 @@
 class Dollar
   attr_accessor :amount
+  private :amount=
 
   def initialize(amount)
     self.amount = amount
   end
 
   def *(multiplier)
-    return Dollar.new(amount * multiplier)
+    Dollar.new(amount * multiplier)
   end
 
   def ==(object)
--- test/test_money.rb
+++ test/test_money.rb
@@ -3,10 +3,8 @@ require "test_helper"
 class MoneyTest < Minitest::Test
   def test_multiplication
     five = Dollar.new(5)
-    product = five * 2
-    assert_equal 10, product.amount
-    product = five * 3
-    assert_equal 15, product.amount
+    assert_equal Dollar.new(10), five * 2
+    assert_equal Dollar.new(15), five * 3
   end
 
   def test_equality
